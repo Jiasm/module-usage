9393ebbe47269e77fd90d41d8755ae710012cb1b
jiashunming <jiashunming@outlook.com>
2018-06-09 16:01:09 +0800

    update: add some examples

---
+++ example/simple-module.js
@@ -0,0 +1,7 @@
+// export default function test() {
+//   console.log('trigger test')
+// }
+
+// export const str = 'hello world'
+
+export default 'hello world'
---
+++ example/simple.js
@@ -0,0 +1,4 @@
+import * as simpleModule from './simple-module.js'
+import * as remoteModule from 'https://blog.jiasm.org/notebook/dist/js/simple-module.js'
+
+console.log(simpleModule, remoteModule)
